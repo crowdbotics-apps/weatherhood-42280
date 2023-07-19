68588986b57987dbf2e94c9494b6c563e8173cd8
Crowdbotics <team@crowdbotics.com>
2023-07-19 15:55:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weatherhood-prd-ai-42280",
+  url: "https://weatherhood-prd-ai-42280.botics.co",
+  api: "https://weatherhood-prd-ai-42280.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
